60a8065acd97054c44dfb19ba1e1494d2632518f
Aldo Iljazi <pllyj@7tags.com>
2013-07-02 18:50:37 +0300

    added git installation & setup & also made the script executable

--- syshack.sh
+++ syshack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# update system
+# system update
 sudo apt-get -f -y install
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -8,3 +8,12 @@ sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 sudo apt-get -y clean
+
+# git installation & setup
+sudo apt-get -y install git
+sudo apt-get -y install git-email
+git config --global user.name "Aldo Iljazi"
+git config --global user.email "pllyj@7tags.com"
+git config --global credential.helper 'cache --timeout=3600'
+git config --global core.editor nano
+git config --global color.ui true
