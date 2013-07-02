9cd9080ae73e506cc0fc1007e349cd511d218dea
Aldo Iljazi <neonsync1@gmail.com>
2013-07-02 18:23:43 +0300

    first commit & update

---
+++ syshack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# update system
+sudo apt-get -f -y install
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
+sudo apt-get -y clean
